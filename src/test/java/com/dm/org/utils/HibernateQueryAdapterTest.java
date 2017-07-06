d2d3134c0acb93727f0dd2b7e530d9da4ec9df93
LuletterSoul <qq313700046@icloud.com>
2017-07-06 13:07:07 +0800

    test util

---
+++ src/test/java/com/dm/org/utils/HibernateQueryAdapterTest.java
@@ -0,0 +1,125 @@
+package com.dm.org.utils; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* HibernateQueryAdapter Tester. 
+* 
+* @author XiangDe Liu qq313700046@icloud.com
+* @since <pre>���� 6, 2017</pre> 
+* @version 1.0 
+*/ 
+public class HibernateQueryAdapterTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: updateStringAssemble(Map<String, Object> updateMappingValues, String classSimpleName) 
+* 
+*/ 
+@Test
+public void testUpdateStringAssemble() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: whereStringAssemble(Map<String, Object> conditionMappingValues) 
+* 
+*/ 
+@Test
+public void testWhereStringAssemble() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getHqlConditionFragmentStringByMapping(Map<String, Object> mapping) 
+* 
+*/ 
+@Test
+public void testGetHqlConditionFragmentStringByMapping() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getHqlUpdateFragmentStringByMapping(Map<String, Object> mapping, String classSimpleName) 
+* 
+*/ 
+@Test
+public void testGetHqlUpdateFragmentStringByMapping() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+/** 
+* 
+* Method: buildUpdateFragmentString(String classSimpleName) 
+* 
+*/ 
+@Test
+public void testBuildUpdateFragmentString() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = HibernateQueryAdapter.getClass().getMethod("buildUpdateFragmentString", String.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: setHqlFragmentPlaceholders(String key, int placeholder) 
+* 
+*/ 
+@Test
+public void testSetHqlFragmentPlaceholders() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = HibernateQueryAdapter.getClass().getMethod("setHqlFragmentPlaceholders", String.class, int.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: propertyTypeResolveAndBuild(String key, Object value, StringBuilder fragmentBuilder, int resolveType) 
+* 
+*/ 
+@Test
+public void testPropertyTypeResolveAndBuild() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = HibernateQueryAdapter.getClass().getMethod("propertyTypeResolveAndBuild", String.class, Object.class, StringBuilder.class, int.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+} 
