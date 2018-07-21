51c14401320553848250b9e5d58a917ca7fc493e
XiangDe <qq313700046@icloud.com>
2018-07-21 16:10:02 +0800

    重构分组策略的代码

--- dm-service/src/main/java/com/vero/dm/service/TeacherService.java
+++ dm-service/src/main/java/com/vero/dm/service/TeacherService.java
@@ -1,14 +1,16 @@
 package com.vero.dm.service;
 
+
 import com.vero.dm.model.Teacher;
+
 
 /**
  * @author XiangDe Liu qq313700046@icloud.com .
- * @version 1.5
- * created in  14:07 2018/7/21.
+ * @version 1.5 created in 14:07 2018/7/21.
  * @since data-mining-platform
  */
 
-public interface TeacherService extends UserService {
+public interface TeacherService extends UserService
+{
     Teacher findTeacherById(String id);
 }
