24ea06ef9826319442dc38f6463dcfbd9758f22f
XiangDe <qq313700046@icloud.com>
2018-08-01 23:28:56 +0800

    修复ResultState的序列化问题

--- dm-model/src/main/java/com/vero/dm/model/enums/ResultState.java
+++ dm-model/src/main/java/com/vero/dm/model/enums/ResultState.java
@@ -1,11 +1,16 @@
 package com.vero.dm.model.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 /**
  * @author XiangDe Liu qq313700046@icloud.com .
  * @version 1.5 created in 22:21 2018/7/24.
  * @since data-mining-platform
  */
 
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum  ResultState implements BaseEnum
 {
     noResult(1,"NO_RESULT","未提交"),
@@ -25,6 +30,17 @@ public enum  ResultState implements BaseEnum
         this.description = description;
     }
 
+    @JsonCreator
+    public static ResultState getItem(int value){
+        for(ResultState item : values()){
+            if(item.getValue() == value){
+                return item;
+            }
+        }
+        return null;
+    }
+
+    @JsonValue
     @Override
     public int getValue()
     {
