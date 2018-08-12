ee6bf4fa6a33ce324999663f00599b13dd1a1760
XiangDe <qq313700046@icloud.com>
2018-08-12 13:04:53 +0800

    doc打包错误

--- dm-security/src/main/java/com/vero/dm/security/credentials/UserProfileAccessor.java
+++ dm-security/src/main/java/com/vero/dm/security/credentials/UserProfileAccessor.java
@@ -1,20 +1,25 @@
 package com.vero.dm.security.credentials;
 
+
 import com.vero.dm.repository.dto.UserDto;
+
 
 /**
  * 从缓存中拉取对应的用户信息
+ * 
  * @author XiangDe Liu qq313700046@icloud.com .
- * @version 1.5
- * created in  13:01 2018/2/10.
+ * @version 1.5 created in 13:01 2018/2/10.
  * @since data-mining-platform
  */
 
-public interface UserProfileAccessor {
+public interface UserProfileAccessor
+{
 
     /**
      * 拉取用户信息
-     * @param accessToken 授权证书
+     * 
+     * @param accessToken
+     *            授权证书
      * @return 用户信息
      */
     UserDto fetchProfile(String accessToken);
